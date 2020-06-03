d15b61bef2e7e2d70b73aa22a086afe0e85831de
PauloMorillo <pauloan@hotmail.com>
2020-06-03 01:31:57 +0000

    [update] added condition to avoid repeat

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -9,30 +9,38 @@ if __name__ == "__main__":
     size = 0
     statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-    def signal_handler(signal, frame):
-        """ Method to handle ctrl-c"""
-        print("File size: {}".format(size))
-        for i in range(len(statusn)):
-            if statusn[i] > 0:
-                print("{}: {}".format(statuses[i], statusn[i]))
-        sys.exit(0)
-    signal.signal(signal.SIGINT, signal_handler)
-    for line in sys.stdin:
-        # print(line)
-        each_argvs = line.split(" ")
-        if len(each_argvs) == 9:
-            status = each_argvs[-2]
-            if status in statuses:
-                i = statuses.index(status)
-                statusn[i] = statusn[i] + 1
-                size = size + int(each_argvs[-1][:-1])
-                if a % 10 == 0:
-                    print("File size: {}".format(size))
-                    for i in range(len(statusn)):
-                        if statusn[i] > 0:
-                            print("{}: {}".format(statuses[i], statusn[i]))
-        a = a + 1
-    if a % 10 is not 0:
+    # def signal_handler(signal, frame):
+    # """ Method to handle ctrl-c"""
+    # print("File size: {}".format(size))
+    # for i in range(len(statusn)):
+    # if statusn[i] > 0:
+    # print("{}: {}".format(statuses[i], statusn[i]))
+    # sys.exit(0)
+    # signal.signal(signal.SIGINT, signal_handler)
+    try:
+        for line in sys.stdin:
+            # print(line)
+            b = 0
+            each_argvs = line.split(" ")
+            if len(each_argvs) == 9:
+                status = each_argvs[-2]
+                if status in statuses:
+                    i = statuses.index(status)
+                    statusn[i] = statusn[i] + 1
+                    size = size + int(each_argvs[-1][:-1])
+                    if a % 10 == 0:
+                        b = 1
+                        print("File size: {}".format(size))
+                        for i in range(len(statusn)):
+                            if statusn[i] > 0:
+                                print("{}: {}".format(statuses[i], statusn[i]))
+            a = a + 1
+        if b is not 1:
+            print("File size: {}".format(size))
+            for i in range(len(statusn)):
+                if statusn[i] > 0:
+                    print("{}: {}".format(statuses[i], statusn[i]))
+    except KeyboardInterrupt:
         print("File size: {}".format(size))
         for i in range(len(statusn)):
             if statusn[i] > 0:
