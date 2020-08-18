28d1176296f705707cac5588ebb1406ab3a3c072
PauloMorillo <paulon@hotmail.com>
2020-08-18 23:56:34 +0000

    [update] complete request

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
-console.log(process.argv[2])
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
+  const characters = JSON.parse(body).characters;
+  if (error != null) {
+    console.error(error);
+  }
+  let a = 0;
+  while (a < characters.length) {
+    request(characters[a], function (error, response, body) {
+      if (error != null) {
+        console.error(error);
+      }
+
+      console.log(JSON.parse(body).name);
+    });
+    a = a + 1;
+  }
 });
