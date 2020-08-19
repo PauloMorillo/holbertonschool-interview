2cf71d545bed283fd8f39e4b2b17c46d1dc4bdb4
PauloMorillo <paulon@hotmail.com>
2020-08-19 01:20:03 +0000

    [fixed] added semistandard for reject

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,10 +4,10 @@ const request = require('request');
 function requesito (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
-      if (error) console.error(error);
-      if (response.statusCode !== 200) {
+      if (error) reject(error);
+      /* if (response.statusCode !== 200) {
         reject('Invalid status code <' + response.statusCode + '>');
-      }
+      } */
       resolve(body);
     });
   });
