22f71b8f3b9a1ed0331903c32e0f3e960a2a8b88
PauloMorillo <paulon@hotmail.com>
2020-08-19 00:50:59 +0000

    [update] added promise and async

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+
+function requesito (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) reject(error);
+      if (response.statusCode !== 200) {
+        reject('Invalid status code <' + response.statusCode + '>');
+      }
+      resolve(body);
+    });
+  });
+}
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], async function (error, response, body) {
   const characters = JSON.parse(body).characters;
-  if (error != null) {
+  if (error !== null) {
     console.error(error);
   }
   let a = 0;
   while (a < characters.length) {
-    request(characters[a], function (error, response, body) {
-      if (error != null) {
-        console.error(error);
-      }
+    try {
+	  const body = await requesito(characters[a]);
+	  console.log(JSON.parse(body).name);
+    } catch (error) {
+	  console.error(error);
+    }
 
-      console.log(JSON.parse(body).name);
-    });
     a = a + 1;
   }
 });
