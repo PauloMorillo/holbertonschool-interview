7741573fbbe49fb6d6020777176f4f5cda44195e
PauloMorillo <pauloan@hotmail.com>
2020-11-18 23:52:00 -0500

    [Update] added directory and script for interview task

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,103 @@
+#include "list.h"
+/**
+ * insert - insert first node
+ * @list: circular linked list
+ * @str: string (malloc'ed string)
+ * Return: new node, or if null if it fails
+ */
+List *insert(List **list, char *str)
+{
+	List *new_node;
+
+	new_node = malloc(sizeof(List));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+
+	new_node->next = new_node;
+	new_node->prev = new_node;
+	*list = new_node;
+
+	return (new_node);
+}
+
+/**
+ * add_top - node at the top
+ * @list: doubly linked list
+ * @str: string - (malloc'ed string)
+ * Return: new node, or if null if it fails
+ */
+List *add_top(List **list, char *str)
+{
+	List *new_node, *temp, *temp2;
+
+	if (list == NULL)
+		return (NULL);
+
+	if (*list == NULL)
+		return (insert(list, str));
+
+	temp2 = *list;
+	new_node = malloc(sizeof(List));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = temp2;
+	new_node->prev = temp2->prev;
+	temp = temp2->prev;
+	temp2->prev = new_node;
+	temp->next = new_node;
+	*list = new_node;
+
+	return (new_node);
+}
+
+/**
+ * add_tail - node at last position
+ * @list: doubly linked list
+ * string - (malloc'ed string)
+ * Return: new node, or NULL if it fails
+ */
+List *add_tail(List **list, char *str)
+{
+	List *new_node, *tmp, *temp2;
+
+	if (list == NULL)
+		return (NULL);
+
+	if (*list == NULL)
+		return (insert(list, str));
+
+	temp2 = *list;
+	new_node = malloc(sizeof(List));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = temp2;
+	new_node->prev = temp2->prev;
+	tmp = temp2->prev;
+	temp2->prev = new_node;
+	tmp->next = new_node;
+
+	return (new_node);
+}
---
+++ 0x1A-double_circular_linked_list/README.md
@@ -0,0 +1,26 @@
+# Holberton Interview
+
+This repo has the exercise to prepare for an interview
+
+## Requirements
+
+Ubuntu(14.04)
+gcc(4.8.4)
+
+## Run program
+
+first we should compile and later we can run
+
+´´´bash
+gcc -Wall -Wextra -Werror -pedantic 0-main.c 0-add_node.c
+
+./a.out 
+
+´´´
+
+
+## Author
+Paulo Morillo
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
